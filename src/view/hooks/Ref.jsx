a03c518e0d7106d90b43b890a0346b27dc2e81c2
Ashine33 <404770266@qq.com>
2023-07-10 15:54:09 +0800

    feat: 使用 ref 来操作 DOM

--- src/view/hooks/Ref.jsx
+++ src/view/hooks/Ref.jsx
@@ -1,31 +1,46 @@
 import { useRef, useState } from "react";
 
+// function Ref() {
+//   const [n, setN] = useState(0);
+
+//   const addN = () => {
+//     setN((x) => x + 1);
+//   };
+
+//   const ref = useRef(10);
+
+//   const addRef = () => {
+//     // ref的current的值会变化，但不会触发页面的重新渲染
+//     ref.current = ref.current + 1;
+//     console.log('ref.current--- ',ref.current )
+//   };
+//   return (
+//     <>
+//       <div style={{ padding: "30px" }}>
+//         <div> {n}</div>
+//         <button onClick={addN}>n+1</button>
+//       </div>
+
+//       <div style={{ padding: "30px" }}>
+//         <div> {ref.current} </div>
+//         <button onClick={addRef}>ref + 1</button>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Ref;
+
 function Ref() {
-  const [n, setN] = useState(0);
 
-  const addN = () => {
-    setN((x) => x + 1);
-  };
-
-  const ref = useRef(10);
-
-  const addRef = () => {
-    ref.current = ref.current + 1;
-    console.log('ref.current--- ',ref.current )
-  };
-  return (
-    <>
-      <div style={{ padding: "30px" }}>
-        <div> {n}</div>
-        <button onClick={addN}>n+1</button>
-      </div>
-
-      <div style={{ padding: "30px" }}>
-        <div> {ref.current}</div>
-        <button onClick={addRef}>ref + 1</button>
-      </div>
-    </>
-  );
+    const inputRef = useRef(null)
+    const handleClick = (e) => {
+      inputRef.current.focus()
+    }
+    return ( <>
+        <input  ref={inputRef}/>
+        <button onClick={handleClick}>聚焦输入框</button>
+    </> );
 }
 
 export default Ref;
