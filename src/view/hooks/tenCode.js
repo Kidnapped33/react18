a6b618fb51c65fa9a6f16e24c54c67587cf7bbe1
Ashine33 <404770266@qq.com>
2023-07-13 18:23:56 +0800

    feat: use Promise.all

--- src/view/hooks/tenCode.js
+++ src/view/hooks/tenCode.js
@@ -1,17 +1,38 @@
 // 10 行代码
 
-const sleep = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+// const sleep = (ms) => {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// };
+
+// const minDelay = async (promise, ms) => {
+//   const [p] = await Promise.all([promise, sleep(ms)]);
+//   return p;
+// };
+
+// const fetchDataMock = () => {
+//   return new Promise((resolve, reject) => setTimeout(resolve, 5000));
+// };
+
+
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 0, 'foo');
+});
+
+const minDelay = async () =>{
+  const p = await Promise.all([promise1, promise2, promise3])
+  return p 
 };
 
-const minDelay = async (promise, ms) => {
-  const [p] = await Promise.all([promise, sleep(ms)]);
-  return p;
-};
+minDelay().then(res => console.log('res---',res))
 
-const fetchDataMock = () => {
-  return new Promise((resolve, reject) => setTimeout(resolve, 5000));
-};
+// setTimeout(()=>{
+//   console.log('xxxx',xxx)
+// },3000)
+
+
 // // use
 // await minDelay(fetchDataMock(), 1000);
 
@@ -35,25 +56,25 @@ const fetchDataMock = () => {
 
 // 复习 promise
 
-new Promise((resolve, reject) => {
-  console.log("Promise");
-  resolve(10000);
-}).then(
-  (data) => {
-    console.log("success"), console.log(data + 1);
-  },
-  (data) => {
-    console.log("fail"), console.log(data + 2);
-  }
-);
+// new Promise((resolve, reject) => {
+//   console.log("Promise");
+//   resolve(10000);
+// }).then(
+//   (data) => {
+//     console.log("success"), console.log(data + 1);
+//   },
+//   (data) => {
+//     console.log("fail"), console.log(data + 2);
+//   }
+// );
 
 
-try {
-  console.log("Promise");
-  const data = Promise.resolve(10000);
-  console.log("success");
-  console.log(data + 1);
-} catch (error) {
-  console.log("fail");
-  console.log(error + 2);
-}
+// try {
+//   console.log("Promise");
+//   const data = Promise.resolve(10000);
+//   console.log("success");
+//   console.log(data + 1);
+// } catch (error) {
+//   console.log("fail");
+//   console.log(error + 2);
+// }
